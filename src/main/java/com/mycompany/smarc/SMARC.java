d33aa28a8bd1ee87cf2a170668eb23ccd7511e1b
voidGustavoNunes <gu-stavo125@hotmail.com>
2023-11-07 16:48:19 -0300

    adicionando interface grafica

---
+++ src/main/java/com/mycompany/smarc/SMARC.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.smarc;
+
+/**
+ *
+ * @author Gustavo
+ */
+public class SMARC {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
